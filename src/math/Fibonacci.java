5593421c9206511c83b2abac1f8319b59857109a
kowserparvez <kowserparvez@gmail.com>
2018-04-08 12:39:16 -0400

    Fibonacci

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -5,7 +5,14 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
-
+        int prev=0;
+        int next=1;
+        int counter =40;
+        for (int n=0;n<counter; n++){
+            System.out.println(prev+" ");
+            prev=prev+next;
+            next=prev-next;
+        }
 
     }
 }
